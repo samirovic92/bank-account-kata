f80c7280b5c6893a10eaf8d157e99e849be7ce30
samir <s.elidrissi1992@gmail.com>
2021-11-14 20:35:04 +0100

    Add Snack Bar to Show Error message

---
+++ front/src/app/shared/component/snack-bar/snack-bar.component.ts
@@ -0,0 +1,13 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_SNACK_BAR_DATA} from "@angular/material/snack-bar";
+
+@Component({
+  selector: 'app-snack-bar',
+  template: ' {{ data}}',
+  styleUrls: ['./snack-bar.component.scss']
+})
+export class SnackBarComponent {
+
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: string) { }
+
+}
